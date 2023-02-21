c5d6345bb71657a639a17e6c9156be4ca72f8389
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-02-21 13:01:45 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+<div>
+  <p>Please, leave fedback</p>
+  <div>
+    <button type="button">Good</button>
+    <button type="button">Neutral</button>
+    <button type="button">Bad</button>
+  </div>
+</div>
+  <div>
+    <h1>Statistics</h1>
+    <ul>
+      <li>Good: { }</li>
+      <li>Neutral: { }</li>
+      <li>Bad: { }</li>
+      <li>Total: { }</li>
+      <li>Positive feedback: { }</li>
+    </ul>
+  </div>
+</>
   );
 };
+
+
