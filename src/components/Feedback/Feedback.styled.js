7246ea3e9bcbd0b64da92a84682aee72a4b63d72
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-02-22 06:13:11 +0200

    animation

--- src/components/Feedback/Feedback.styled.js
+++ src/components/Feedback/Feedback.styled.js
@@ -7,12 +7,25 @@ export const ButtonContainer = styled.div`
 
 export const ButtonG = styled.button`
   background-color: #66cd00;
+  transition: all 500ms ease;
+  :hover {
+	cursor: pointer;
+  transform: scale(1.15);
+}
 `;
 
 export const ButtonN = styled.button`
   background-color: #e9d66b;
+  transition: all 500ms ease;
+  :hover {
+	cursor: pointer;
+  transform: scale(1.15);
 `;
 
 export const ButtonB = styled.button`
   background-color: #ff7256;
+  transition: all 500ms ease;
+  :hover {
+	cursor: pointer;
+  transform: scale(1.15);
 `;
